977de67cb84d4439b090e4e4e262c695e7a195c9
zakir1109022 <zakirhasan1109022@gmail.com>
2022-09-24 23:11:32 +0600

    Setup project structure

---
+++ RabbitMQ.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RabbitMQ.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ RabbitMQ.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RabbitMQ.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
